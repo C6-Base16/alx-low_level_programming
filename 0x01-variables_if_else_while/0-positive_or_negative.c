77161d3e5eca2d3d809a39fdeeb84ac4f16f2e0e
C6-Base16 <t.h.e.data.king.01@gmail.com>
2023-06-20 05:26:22 -0700

    sign_number

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+  *main -program start
+  *Description: number sign checcker
+  *Return: 0 (success)
+  */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+		printf(" %d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf(" %d is Zero\n", n);
+	}
+	else
+	{
+		printf(" %d is negative\n", n);
+	}
+	return (0);
+}

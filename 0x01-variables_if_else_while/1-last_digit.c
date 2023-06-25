7025e7bd47512e26ab4d54c4704f0b71899c993d
C6-Base16 <t.h.e.data.king.01@gmail.com>
2023-06-25 12:37:39 -0700

    %_operator

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 /**
   *main - program start
   *Description: number comparison
@@ -8,9 +9,19 @@
 int main(void)
 {
 	int n;
+	int ld;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
+	ld = n % 10;
+	if ( ld > 5){
+		printf("Last digit of %d is %d and is greater than 5\n", n, ld);
+	}
+	else if ( ld == 0){
+		printf("Last digit of %d is %d and is 0\n", n, ld);
+	}
+	else{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, ld);
+	}
 	return (0);
 }

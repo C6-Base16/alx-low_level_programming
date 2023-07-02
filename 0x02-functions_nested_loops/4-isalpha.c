3444333820c0fbfdf93157190a9b665d9fe6150d
C6-Base16 <t.h.e.data.king.01@gmail.com>
2023-07-02 10:51:48 -0700

    is_alpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,11 +5,12 @@
   * @c: the characte to be checked
   * Return: 1 for alphabet or 0 for other
   */
+
 int _isalpha(int c)
 {
 	if ((c >= 65 && c  <= 97) || (c >= 97 && c <= 122))
-			{
-			return (1);
-			}
+	{
+		return (1);
+	}
 	return (0);
 }

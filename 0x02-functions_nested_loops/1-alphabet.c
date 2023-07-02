97e92a9eaed2b47cf28d0a64a615fbaa1928a4c7
C6-Base16 <t.h.e.data.king.01@gmail.com>
2023-07-02 07:30:00 -0700

    alpha

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,14 @@
 #include "main.h"
 /**
-  *main - start program
-  *print_alphabet : Print ascii 97-122
-  *Return: 0
+ *print_alphabet - Print ascii 97-122
+  *Return void
   */
 
 void print_alphabet(void)
 {
 	char ch;
 
-	for (ch ='a'; ch <= 'z'; ch++)
+	for (ch = 'a'; ch <= 'z'; ch++)
 	{
 		_putchar(ch);
 	}

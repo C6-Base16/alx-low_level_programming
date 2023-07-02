3e804b95738da9d671c798f2ecea3f9b7a26bdba
C6-Base16 <t.h.e.data.king.01@gmail.com>
2023-07-02 11:02:26 -0700

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+  * print_sign - prints sign
+  * @n: number to be checked
+  * Return: 1 for positive, -1 for negative, 0 for other
+  */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
+}
+

548e53cd6f3a6cefe23cbfaa5ec8d5bdedd512a6
C6-Base16 <t.h.e.data.king.01@gmail.com>
2023-07-03 09:54:40 -0700

    memory

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+  * _memset - fill a block of memory with a value
+  * @s: address to be filled
+  * @b: value
+  * @n: number of bytes
+  * 
+  * Return: array for n bytes
+  */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0;
+
+	for (; i > 0; i++)
+	{
+		s[i] = b;
+		n--;
+	}
+	return (s);
+}
